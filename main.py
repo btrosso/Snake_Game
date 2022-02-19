40520a96e6a6f156a624cfc13a5eb1dcbd0d192f
btrosso <51491995+btrosso@users.noreply.github.com>
2022-02-18 20:06:42 -0600

    update to main/scoreboard/snake
    added high score functionality

--- main.py
+++ main.py
@@ -34,17 +34,20 @@ while game_is_on:
         print("nom nom nom")
         food.refresh()
         snake.extend()
-        scoreboard.update_score()
+        scoreboard.increase_score()
+
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
+    scoreboard.update_scoreboard()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -11,15 +11,22 @@ class Scoreboard(Turtle):
         self.goto(0, 270)
         self.color("white")
         self.score = 0
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.high_score = 0
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def update_score(self):
-        self.score += 1
+    def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over.", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game Over.", align=ALIGNMENT, font=FONT)
 
+    def increase_score(self):
+        self.score += 1
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
 
